ec44f2956d269d520a49a908b6c30701719a4af3
BEOOM <wnsqja0512@naver.com>
2022-06-14 12:20:24 +0900

    Init: 원격에 올리는 중

---
+++ Desktop/nativeApp/src/Main/Main.js
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Main</Text>
+      <Text>Main</Text>
+      <Text>Main</Text>
+      <Text>this is Main</Text>
+      <Text>Main</Text>
+      <Text>Main</Text>
+      <Text>Main</Text>
+    </View>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({});
